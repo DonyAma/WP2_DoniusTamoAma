9bd049aaf2dffb16d3c148ed03e823816ea7e430
DonyAma <amadony18@gmail.com>
2022-03-31 00:27:37 +0700

    Menambahkan isi file v_about.php beserta functionnya

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Dony</li>
+        <li>Nama Belakang : Ama</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan maulana hasanuddin poris gaga</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>NTT</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
