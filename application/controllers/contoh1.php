380ff0a2bb03521056054a021ff884d1c4ff60f2
DonyAma <amadony18@gmail.com>
2022-03-30 19:47:51 +0700

    Menambahkan isi contoh1.php yaitu BIODATA DIRI.

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Donius Tamo Ama
+ Saya tinggal di daerah Tanggerang Kota
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+    }
+}
