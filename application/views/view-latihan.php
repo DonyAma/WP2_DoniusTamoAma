3221a0b0dc2ca4145623ffe23224c4743b6fd323
DonyAma <amadony18@gmail.com>
2022-03-30 20:48:39 +0700

    Menambahkan isi file view-latihan.php yaitu penggunakan Controller, View dan Model.

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    <h3> Halo Kawan.. Yuk kita belajar web programming...!!!<br></h3>
+    <h4> Nilai 1 = <?= $nilai1; ?>
+        Nilai 2 = <?= $nilai2; ?>
+        ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+                                                                    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?></h4>
+</body>
+
+</html>
