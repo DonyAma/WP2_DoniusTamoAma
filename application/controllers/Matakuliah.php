67ba8e6a73cd8364cf901d85c2a47ec41d42242c
DonyAma <amadony18@gmail.com>
2022-04-05 21:33:00 +0700

    Memuat Library Form_validation

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -5,14 +5,35 @@ class Matakuliah extends CI_Controller
     {
         $this->load->view('view-form-matakuliah');
     }
-
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks'),
-        ];
-        $this->load->view('view-datamatakuliah', $data);
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Kode Matakuliah Harus diisi',
+                'min_lenght' => 'Kode terlalu pendek'
+            ]
+        );
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Nama Matakuliah Harus diisi',
+                'min_lenght' => 'Nama terlalu pendek'
+            ]
+        );
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+            $this->load->view('view-datamatakuliah', $data);
+        }
     }
 }
